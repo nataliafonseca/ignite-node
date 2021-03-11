16cf4cf81c73cf72b63598ccc143c126cd4998c7
Natália Fonseca <nataliabf@outlook.com>
2021-03-11 00:33:12 -0300

    Primeiro Projeto: FinAPI - Validaçoes, Listando Extrato e Middlewares

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -7,19 +7,48 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found!' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
 
   customers.push({
+    id: uuidv4(),
     cpf,
     name,
-    id,
     statement: [],
   });
 
   return response.status(201).json(customers);
+});
+
+app.use(verifyIfExistsAccountCPF);
+
+app.get('/statement', (request, response) => {
+  const { customer } = request;
+
+  return response.status(200).json(customer.statement);
 });
 
 app.listen(3333);
