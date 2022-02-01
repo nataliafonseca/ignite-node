02d3a078fc34d88f84e96c42c80d9a00faa9fad0
Natália Fonseca <nataliabf@outlook.com>
2022-02-01 14:45:47 -0300

    Projeto: RentalX - Configuração de produção: Adicionando configuração do cors

--- rentalx/src/shared/infra/http/app.ts
+++ rentalx/src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import upload from '@config/upload';
 import '@shared/container';
 import { handleErrors } from '@shared/infra/http/middleware/handleErrors';
 import createConnection from '@shared/infra/typeorm';
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
+app.use(cors({ origin: '*' }));
 app.use(router);
 app.use(handleErrors);
 
